9dcdf53b33dbbedbe80fdb185f1d9055c810d211
Arham Garg <arhamvgarg@gmail.com>
2026-04-09 15:34:06 +0530

    style: Refactor SynchronisedCounter for improved code structure and readability

--- src/synchronisation/SynchronisedCounter.cpp
+++ src/synchronisation/SynchronisedCounter.cpp
@@ -1,27 +1,35 @@
+#ifndef SYNCHRONISEDCOUNTER_H
+#define SYNCHRONISEDCOUNTER_H
+
 #include <atomic>
-#include <iostream>
-#include <thread>
 using namespace std;
 
 class SynchronisedCounter {
-public:
+  public:
   void incrementBy(int value) { count_.fetch_add(value, memory_order_relaxed); }
 
   int getCount() const { return count_.load(memory_order_relaxed); }
 
-private:
+  private:
   atomic<int> count_{0};
 };
+
+#endif
+
+#include <iostream>
+#include <thread>
 
 int main() {
   SynchronisedCounter counter;
   thread t1([&counter]() {
-    for (int i = 0; i < 1000; ++i)
+    for (int i = 0; i < 1000; ++i) {
       counter.incrementBy(2);
+    }
   });
   thread t2([&counter]() {
-    for (int i = 0; i < 1000; ++i)
+    for (int i = 0; i < 1000; ++i) {
       counter.incrementBy(2);
+    }
   });
   t1.join();
   t2.join();
